ecc593d773487245ee68020eca5f3d73de8f7379
toki513 <tokitahmidkhan@gmail.com>
2024-12-11 23:20:48 +0600

    updated

--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
 
-document.getElementById('home-btn').addEventListener('click', function () { 
+document.getElementById('home-btn').addEventListener('click', function (event) { 
     window.location.href = 'index.html'
 })
